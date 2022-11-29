59ec67c1d0e69aa298c581b7be35453a3bf9432f
Renata Reinartz <rcreinartz@gmail.com>
2022-11-28 23:16:51 -0700

    final project

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click",() => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    // hamburger.classList.toggle("active");
+    // navMenu.classList.toggle("active");
 })
+
+document.querySelectorAll(".navLink").forEach(n => n
+.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
